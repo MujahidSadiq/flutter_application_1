e1e060394e29274b18252903c555882acef100dc
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-02-22 19:13:05 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, World!'),
+        ),
+        body: Center(
+          child: Text('Hello world',
+          style: TextStyle(fontSize: 49,fontWeight: FontWeight.bold),),
+        ),
+      ),
+    );
+  }
+}
